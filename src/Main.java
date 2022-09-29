a47fb99ba5c0befc5803ee46cc8de504fdb6c893
Engel62 <sr.engel@bk.ru>
2022-09-29 15:33:54 +0300

    урок 2 задание 4

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 2.2");
+        var friends = 19;
+        System.out.println(friends);
+        friends = friends * 2;
+        System.out.println(friends);
+        friends = friends / 7;
+        System.out.println(friends);
+    }
+}
